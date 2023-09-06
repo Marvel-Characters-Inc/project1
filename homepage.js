b7acf4ab6126f83225fe96c5b61001c7b0aa2a65
Lenzlee Wheeler <lwheeler@luc.edu>
2023-09-06 18:39:53 -0500

    updated html and js

--- homepage.js
+++ homepage.js
@@ -2,13 +2,22 @@
 //testing API
 function test() {
     var apiMarvel =
-      "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67d8c18582668ccfcdaa772663f6e18e&hash=b2a33e8def4a19cbf8d5f13f82ec50e5";
+      "https://www.superheroapi.com/api.php/10104712034177731/346";
 
     fetch(apiMarvel).then(function (response) {
       console.log(response);
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
+
+          var characterData = $('<div>');
+          characterData.addClass('col s3');
+          var charID = data.id
+          var charName = $("<h5>").text(data.name);
+          var charImage = $('<img>').attr('src', data.image.url);
+          characterData.append(charName).append(charImage);
+          $('#charactersBox').append(characterData);
+
         });
       }
     });
