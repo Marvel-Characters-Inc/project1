c10e4f07310d7667f05f17fa9179d117d87deacc
Your Name <ScottyJirik@MacBook-Pro.local>
2023-09-06 20:51:42 -0500

    Started on JS for the page, have gotten the API calls and info down, going to work on image next
    Co-authored by: jguzman12310@yahoo.com

--- assets/js/interior-script.js
+++ assets/js/interior-script.js
@@ -1,22 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {
-      // specify options here
-    });
+var superHero = document.querySelector("#hero-name")
+var realName = document.querySelector("#real-name")
+var originCity = document.querySelector("#city")
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {
+    // specify options here
   });
+});
 
 
-    function getApi() {
-      var requestUrl = 'https://superheroapi.com/api.php/3008242709313318/659/biography';
-      var chosenCharacter = ''
-      var apiUrl = 'https://superheroapi.com/api.php/3008242709313318/' + chosenCharacter ;
-      fetch(requestUrl)
-      .then(function (response) {
-      return response.json();
-      })
-      .then(function (data) {
-        displayCharInfo(data,)
-      console.log(data)
-      })}
-      getApi();
-      
+function getApi() {
+  var requestUrl = 'https://superheroapi.com/api.php/3008242709313318/659';
+  var chosenCharacter = ''
+  var apiUrl = 'https://superheroapi.com/api.php/3008242709313318/' + chosenCharacter;
+  fetch(requestUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayCharInfo(data)
+          console.log(data)
+        })
+      }
+    })
+};
+
+
+
+
+var displayCharInfo = function (hero) {
+  var heroName = hero.name
+  var heroRealName = hero.biography.aliases[0]
+  var heroOriginCity = hero.work.base
+  
+  superHero.textContent = heroName
+  realName.textContent = heroRealName
+  originCity.textContent = heroOriginCity
+
+};
+
+getApi();
