2a6320c0d97c9616836a207d8b97b65d001c28d7
Your Name <ScottyJirik@MacBook-Pro.local>
2023-09-06 21:20:41 -0500

    Added an image variable, trying to get it to work

--- assets/js/interior-script.js
+++ assets/js/interior-script.js
@@ -1,4 +1,5 @@
 var superHero = document.querySelector("#hero-name")
+var heroImage = document.querySelector('#hero-image')
 var realName = document.querySelector("#real-name")
 var originCity = document.querySelector("#city")
 
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function getApi() {
-  var requestUrl = 'https://superheroapi.com/api.php/3008242709313318/659';
+  var requestUrl = 'https://superheroapi.com/api.php/3008242709313318/346';
   var chosenCharacter = ''
   var apiUrl = 'https://superheroapi.com/api.php/3008242709313318/' + chosenCharacter;
   fetch(requestUrl)
@@ -36,13 +37,14 @@ function getApi() {
 
 var displayCharInfo = function (hero) {
   var heroName = hero.name
-  var heroRealName = hero.biography.aliases[0]
+  var heroRealName = hero.biography.publisher
   var heroOriginCity = hero.work.base
+  var imageHero = hero.image.url
   
   superHero.textContent = heroName
   realName.textContent = heroRealName
   originCity.textContent = heroOriginCity
-
+  heroImage.textContent = imageHero
 };
 
 getApi();
