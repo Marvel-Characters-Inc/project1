8135f1ee07a1db16556da96d8be175b2e2f27dc4
Flunder620 <jguzman12310@yahoo.com>
2023-09-06 19:49:14 -0500

    added api call function
    Co-authored-by: jirikscotty@gmail.com

--- assets/js/interior-script.js
+++ assets/js/interior-script.js
@@ -4,4 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
       // specify options here
     });
   });
-          
+
+
+    function getApi() {
+      var requestUrl = 'https://superheroapi.com/api.php/3008242709313318/659/biography';
+      var chosenCharacter = ''
+      var apiUrl = 'https://superheroapi.com/api.php/3008242709313318/' + chosenCharacter ;
+      fetch(requestUrl)
+      .then(function (response) {
+      return response.json();
+      })
+      .then(function (data) {
+        displayCharInfo(data,)
+      console.log(data)
+      })}
+      getApi();
+      
