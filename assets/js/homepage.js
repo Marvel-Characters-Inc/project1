d443eb6195e0a681c6b242a2544c803fc6d8f8a9
Lenzlee Wheeler <lwheeler@luc.edu>
2023-09-06 20:52:01 -0500

    edited README file, updated homepage JS file with pseudo code
    Co-authored by cervantes.lila14@gmail.com

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,21 @@
+//define variables
+var APIKey = "10104712034177731";
+var characterID = "346";
+
+// define variables for Theme Arrays
+      //Theme 1 (OG Avengers) array should contain ids=Thor(659),Hulk(332),Iron Man (346), Captain America (149)
+
+      //Theme 2 (Villains) array should contain characters/id = Loki(414), Magneto(423), Thanos (655), Green Goblin (299)
+
+      //Theme 3 (Sheroes) array should contain characters/id = Black Widow (107), Scarlet Witch (579), Captain Marvel (156), Jean Grey (356)
+
+      //Theme 4 (Heroes) array should contain characters/id = Black Panther (106), Ant Man (31), Spider Man (620), Wolverine (717)
 
 //testing API
 function test() {
-    var apiMarvel =
-      "https://www.superheroapi.com/api.php/10104712034177731/346";
+    var apiSuperHero = "https://www.superheroapi.com/api.php/" + APIKey + "/" + characterID;
 
-    fetch(apiMarvel).then(function (response) {
+    fetch(apiSuperHero).then(function (response) {
       console.log(response);
       if (response.ok) {
         response.json().then(function (data) {
@@ -24,4 +35,10 @@ function test() {
   }
   test();
 
-// on page load, query API and display 
+// First Function should be on page load, query SuperHeros API and match character ids with theme selected
+
+  //Sub Function should be an event listener for what theme the user clicks and should display characters for each theme based on what is defined in each array/theme (see theme arrays defined above)
+
+//Second Function should be event listener for what character the user clicks
+
+//Third Function should be appending the character id value to the interior page URL
